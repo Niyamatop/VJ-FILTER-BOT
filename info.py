184c38650a75f10672d5214ababfe7988914a1ae
Dark souls <87437573+Niyamatop@users.noreply.github.com>
2024-07-04 15:51:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://DarkSouls:DarkSouls@cluster0.x8fz2sg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Venom786:Venom786@venom123.xt8sm46.mongodb.net/?retryWrites=true&w=majority&appName=Venom123")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Venom123")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 # Premium And Referal Settings
